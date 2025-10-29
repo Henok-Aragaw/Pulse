f064cd903a477dc9e235c88672dcf35d0974702a
Henok-Aragaw <henokaragaw17@gmail.com>
2025-10-29 07:11:54 -0900

    auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,3 @@
-export const runtime = "nodejs";
-
 import { APIError, betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@/generated/prisma";
@@ -8,66 +6,63 @@ import { createAuthMiddleware } from "better-auth/api";
 import { normalizeName, VALID_DOMAIN } from "./utils";
 
 const prisma = new PrismaClient();
-
-let authInstance: ReturnType<typeof betterAuth> | null = null;
-
-try {
-  authInstance = betterAuth({
+export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-      provider: "mongodb",
+        provider: "mongodb", 
     }),
     emailAndPassword: {
-      enabled: true,
-      minPasswordLength: 6,
-      autoSignIn: false,
+      enabled:true,
+      minPasswordLength:6,
+      autoSignIn:false,
     },
     socialProviders: {
       google: {
         clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.NEXT_SECRET_GOOGLE_CLIENT_SECRET as string,
-      },
+      }
     },
-    hooks: {
-      before: createAuthMiddleware(async (ctx) => {
-        if (ctx.path === "/sign-up/email") {
+    hooks:{
+      before:createAuthMiddleware(async(ctx) => {
+        if(ctx.path === '/sign-up/email'){
           const email = String(ctx.body.email);
           const domain = email.split("@")[1];
 
-          if (!VALID_DOMAIN().includes(domain)) {
-            throw new APIError("BAD_REQUEST", {
-              message: "Invalid domain. Please use a valid email.",
-            });
+          if(!VALID_DOMAIN().includes(domain)){
+              throw new APIError("BAD_REQUEST", {
+                        message:'Invalid domain, Please use a valid email.'
+              });
           }
 
           const name = normalizeName(ctx.body.name);
+          
           return {
             context: {
               ...ctx,
-              body: { ...ctx.body, name },
-            },
-          };
+              body:{
+                ...ctx.body,
+                name
+              }
+            }
+          }
         }
-      }),
+      })
     },
     session: {
-      expiresIn: 30 * 24 * 60 * 60,
+       expiresIn: 30 * 24 * 60 * 60,
     },
     account: {
-      accountLinking: {
-        enabled: false,
-      },
+      accountLinking:{
+        enabled:false
+      }
     },
-    user: {
+    user:{
       changeEmail: {
-        enabled: true,
-      },
+        enabled: true
+      }
     },
-    plugins: [nextCookies()],
-  });
-} catch (error) {
-  console.error("❌ BetterAuth initialization failed:", error);
-}
+    plugins: [
+      nextCookies()
+    ]
+});
 
-export const auth = authInstance!;
-
-export type ErrorCode = keyof typeof auth.$ERROR_CODES | "unknown";
+export type ErrorCode = keyof typeof auth.$ERROR_CODES | 'unknown'
